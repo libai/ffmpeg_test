12c3251f45c0001e97114b3b14a34ad5f2d51893
caoxb <caoxb@ichaotu.com>
2015-03-25 14:41:13 +0800

    修改文件编码

--- decode.h
+++ decode.h
@@ -7,7 +7,7 @@
 typedef void(*video_callback)(uint8_t *data, int size, int64_t pts);
 typedef void(*audio_callback)(uint8_t *data, int size, int64_t pts);
 /*
-*¿ªÊ¼½âÂë
+*å¼€å§‹è§£ç 
 retrun 1 success 0 fail
 */
 int decode_start(char * url, video_callback* video_callback(uint8_t *data, int size, int64_t pts), audio_callback* audio_callback(uint8_t *data, int size, int64_t pts));
